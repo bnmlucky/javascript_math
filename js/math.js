1f3fb49fe69955bd2c38da6394cc469539d7f515
Natalia Titova <bnmlucky@gmail.com>
2020-04-26 17:33:56 -0400

    calculated the number of seconds in a day

---
+++ js/math.js
@@ -0,0 +1,8 @@
+const secsPerMin = 60;
+const minsPerHour = 60;
+const hoursPerDay = 24;
+const daysPerWeek = 7;
+const weeksPerYear = 52;
+const secondsPerDay = secsPerMin * minsPerHour * hoursPerDay;
+
+console.log(`There are ${secondsPerDay} seconds in a day.`);
